ab86a99c5e2753defb88641e84160f6235926ba3
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-04-09 09:45:24 -0400

    Update App.vue

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -12,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/browse">Browse</RouterLink>
+        <RouterLink to="/shoppingcart">Cart</RouterLink>
       </nav>
     </div>
   </header>
