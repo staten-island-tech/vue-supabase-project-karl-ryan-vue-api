fb273608b5487d67fb532a0e1f33eb47f6f10261
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-05-01 09:56:25 -0400

    Update BrowseCata.vue

--- my-app/src/components/BrowseCata.vue
+++ my-app/src/components/BrowseCata.vue
@@ -4,7 +4,7 @@ const supabaseUrl = 'https://zqqcwuxpkpyushoxzsha.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxcWN3dXhwa3B5dXNob3h6c2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNjYzMzAsImV4cCI6MjAyNzY0MjMzMH0.6APkrE0upj7PPo-yW63TaE41kJT_CFAzR_F7aafXkfI'
 const supabase = createClient(supabaseUrl, supabaseKey)
 let { data, error } = await supabase
-  .from('products-germany')
+  .from('tanks')
   .select('tank_name')
 console.log({data,error})
 
