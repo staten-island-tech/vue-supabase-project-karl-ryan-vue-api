73a87552d13a8a18f650368719acccae2567bb89
Shenixx <113193025+Shenixx@users.noreply.github.com>
2024-05-16 09:42:50 -0400

    eee

--- my-app/src/views/BrowseView.vue
+++ my-app/src/views/BrowseView.vue
@@ -1,8 +1,8 @@
 <template>
   <h2> tanks</h2>
   <BrowseCata
-    v-for="tank in tank"
-    :tank= "tank"
+    v-for="tank in tanks"
+    :tank= "tank_name"
   />
 </template>
 
@@ -12,7 +12,7 @@ import { createClient } from '@supabase/supabase-js'
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
-let tank = ref("");
+let tanks = ref();
 async function getTank() {
   const supabaseUrl = 'https://zqqcwuxpkpyushoxzsha.supabase.co'
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxcWN3dXhwa3B5dXNob3h6c2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNjYzMzAsImV4cCI6MjAyNzY0MjMzMH0.6APkrE0upj7PPo-yW63TaE41kJT_CFAzR_F7aafXkfI'
@@ -20,13 +20,13 @@ async function getTank() {
   const { data, error } = await supabase
     .from('tanks')
     .select('tank_name')
-    tank.value = data
-  console.log({ data, error })
+    tanks.value = data
+  // console.log({ data, error })
 }
 
 onMounted(async () => {
   await getTank();
-  console.log(tank)
+  console.log(tanks)
 })
 
 </script>
