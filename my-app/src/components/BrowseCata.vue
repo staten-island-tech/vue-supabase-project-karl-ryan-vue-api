f6ee964bc1eb763423ea0ab62dfbb1a53fb14d61
justthatonedude <113192774+justthatonedude@users.noreply.github.com>
2024-05-20 10:00:06 -0400

    Reapply "Merge branch 'karl' into ham"
    This reverts commit 513a899caa0bfa560a53650c93c3c37cff5ca57c.

---
+++ my-app/src/components/BrowseCata.vue
@@ -0,0 +1,33 @@
+<template>
+    <RouterLink :to="tankPath" class = "cardSet" v-if= "tank" >
+      <h2> {{ tank.tank_name }}</h2>
+      <h3> Price: {{ tank.price }} Silver Lions</h3>
+    </RouterLink>
+</template>
+
+<script setup>
+import {computed} from "vue";
+import { RouterLink } from "vue-router";
+const props = defineProps({
+  tank: Object,
+});
+
+console.log(props.tank);
+
+const tankPath = computed(() => {
+  return (props.tank)
+});
+</script>  
+
+<style scoped>
+  .cardSet {
+  background-color: rgba(2, 2, 2, 0.596);
+  border: 2px solid brown;
+  margin: 3%;
+  padding: 2.6%;
+  text-align: center;
+  flex-basis: 15%;
+  width: 18%;
+  color: white
+}
+</style>
