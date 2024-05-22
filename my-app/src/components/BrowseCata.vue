0c340e4dbdf120dcceaa4266ab68278768e30421
phanther1 <144461089+phanther1@users.noreply.github.com>
2024-05-22 09:53:15 -0400

    cart funtion (kinda)

--- my-app/src/components/BrowseCata.vue
+++ my-app/src/components/BrowseCata.vue
@@ -2,11 +2,17 @@
     <RouterLink :to="tankPath" class = "cardSet" v-if= "tank" >
       <h2> {{ tank.tank_name }}</h2>
       <h3> Price: {{ tank.price }} Silver Lions</h3>
+      <h3>Add: {{ clicked }}</h3>
     </RouterLink>
+    <div class="amount">
+    <button @click="decrement()"> - </button>
+    <button @click="addToCart()"> Add to Cart </button>
+    <button @click="increment()"> + </button>
+    </div>
 </template>
 
 <script setup>
-import {computed} from "vue";
+import {ref, computed} from "vue";
 import { RouterLink } from "vue-router";
 const props = defineProps({
   tank: Object,
@@ -18,7 +24,28 @@ const tankPath = computed(() => {
   // return (props.tank)
   return `/browse/${props.tank.tank_name}`;
 });
-</script>  
+
+let clicked = ref(0);
+
+function increment() {
+  clicked.value++;
+}
+
+function decrement() {
+  clicked.value = Math.max(clicked.value - 1, 0);
+}
+
+function addToCart() {
+  if (clicked.value > 0) {
+  cartValue({ name: props.tank.name, price: props.tank.price, nation: props.tank.nation, quantity: clicked.value });
+  clicked.value = 0;
+  }
+    else {
+    alert('There is 0 items selected. Please reselect. You cannot escape the snail...');
+  }
+}
+
+</script>
 
 <style scoped>
   .cardSet {
@@ -30,5 +57,12 @@ const tankPath = computed(() => {
   flex-basis: 15%;
   width: 18%;
   color: white
+}
+
+.amount {
+  display: flex;
+  flex-basis: 10%;
+  padding: 10%;
+  margin: 0 auto;
 }
 </style>
