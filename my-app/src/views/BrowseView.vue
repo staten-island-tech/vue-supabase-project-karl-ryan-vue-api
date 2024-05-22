58b605802a057997e2c301d557344e5cd5a3e72b
Karl F <144461089+phanther1@users.noreply.github.com>
2024-05-21 22:15:02 -0400

    added filter, sob

--- my-app/src/views/BrowseView.vue
+++ my-app/src/views/BrowseView.vue
@@ -1,7 +1,13 @@
 <template>
+  <nav class="filter">
+    <select v-model="selectedNation">
+        <option value="">All Nations</option>
+        <option v-for="nation in nations" :key="nation" :value="nation">{{ nation }}</option>
+      </select>
+  </nav>
   <div class = "flexbox">
   <BrowseCata
-    v-for="tank in tanks"
+    v-for="tank in filteredTanks"
     :key="tank" 
     :tank="tank"
   />
@@ -11,11 +17,14 @@
 <script setup>
 import BrowseCata from '@/components/BrowseCata.vue';
 import { createClient } from '@supabase/supabase-js'
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
 const route = useRoute();
 
-let tanks = ref([]);
+const tanks = ref();
+const selectedNation = ref('');
+const nations = ref();
+
 const getTank = async () => {
   const supabaseUrl = 'https://zqqcwuxpkpyushoxzsha.supabase.co'
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpxcWN3dXhwa3B5dXNob3h6c2hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIwNjYzMzAsImV4cCI6MjAyNzY0MjMzMH0.6APkrE0upj7PPo-yW63TaE41kJT_CFAzR_F7aafXkfI'
@@ -28,10 +37,17 @@ const getTank = async () => {
     console.error (error);
   } else {
     tanks.value = data;
+    nations.value = [...new Set(data.map(tank => tank.nation))];
   }
 };
-    
-  // console.log({ data, error })
+ // console.log({ data, error })
+
+const filteredTanks = computed(() => {
+  if (!selectedNation.value) {
+    return tanks.value;
+  }
+  return tanks.value.filter(tank => tank.nation === selectedNation.value);
+});
 
 onMounted(getTank);
 
@@ -53,5 +69,10 @@ onMounted(getTank);
   width: 100vw;
   justify-items: center;
   justify-content: center;
+}
+
+.filter {
+  justify-content: center;
+  display: flex;
 }
 </style>
