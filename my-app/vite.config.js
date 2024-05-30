592566a04ae1f503e3cc8f7bbf3b2cf8c8bf8579
justthatonedude <113192774+justthatonedude@users.noreply.github.com>
2024-05-30 09:39:38 -0400

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  },
   plugins: [
     vue(),
   ],
